e63bb096af423edbef2b37b6cd35f9fd911a1524
Vontroy <fengxinyu@pku.edu.cn>
2016-09-01 09:35:42 +0800

    Update POJ 1363 Rails.cpp

--- POJ 1363 Rails.cpp
+++ POJ 1363 Rails.cpp
@@ -6,41 +6,45 @@ using namespace std;
 
 int const maxn = 1005;
 
-int main() 
+int main()
 {
-	int n;
-	int num[maxn];
-	int origin[maxn];
-	while( scanf( "%d", &n ), n ) {
-		memset( origin, 0, sizeof( origin ) );
+    int n;
+    int num[maxn];
+    int origin[maxn];
 
-		while( scanf( "%d", &num[1] ), num[1] ) {
-			for( int i = 2; i <= n; i++ )
-				scanf( "%d", &num[i] );
-			int top = 1;
-			int seq = 2;
-			origin[1] = 1;
-			bool flag = true;
-			for( int i = 1; i <= n; i++ ) {
-				while( true ) {
-					if( origin[top] == num[i]) {
-						origin[top] = 0;
-						top--;
-						break;
-					}
-					else {
-						top++;
-						origin[top] = seq++;
-						if( top > n ) {
-							flag = false;
-							break;
-						}
-					}
-				}
+    while( scanf( "%d", &n ), n ) {
+        memset( origin, 0, sizeof( origin) );
+
+        while( scanf( "%d", &num[1] ), num[1] ) {
+            for( int i = 2; i <= n; i++ ) {
+                scanf( "%d", &num[i] );
             }
-			printf( "%s\n", flag ? "Yes" : "No" );
-		}
-		puts( "" );
-	}
-	return 0;
+
+            int top = 1;
+            int seq = 2;
+            origin[1] = 1;
+            bool flag = true;
+
+            for( int i = 1; i <= n; i++ ) {
+                while( true ) {
+                    if( origin[top] == num[i] ) {
+                        origin[top] = 0;
+                        top--;
+                        break;
+                    }
+                    else {
+                        top++;
+                        origin[top] = seq++;
+                        if( top > n ) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            printf( "%s\n", flag ? "Yes" : "No" );
+        }
+        puts("");
+    }
+    return 0;
 }
